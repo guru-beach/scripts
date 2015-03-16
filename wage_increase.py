2fbbef93dd2506c576db8a18d1c806d753b8601b
Jake Lundberg <jlundberg@adconion.com>
2015-03-16 11:17:43 -0700

    master: better descriptive names

--- wage_increase.py
+++ wage_increase.py
@@ -5,19 +5,20 @@ wage = 9.32
 # New minimum wage
 new_wage = 15
 # Employees working
-employees = 10
+employees = 20
 # Average price transaction
+# Units are monetary (e.g. $)
 avg_price = 40
 # Customers per hour
-turnover = 20
+customers_per_hour = 20
 
-def wage_increase(wage, new_wage, employees, avg_price, turnover):
+def wage_increase(wage, new_wage, employees, avg_price, customers_per_hour):
   wage_delta = (new_wage - wage)
   increase_per_hour =  (new_wage - wage) * employees
-  hour_income = turnover * avg_price
-  new_revenue_target =  hour_income + increase_per_hour
-  increase_per_customer =  new_revenue_target/turnover - avg_price
-  pct_increase_customer = increase_per_customer/avg_price * 100
+  hourly_income = customers_per_hour * avg_price
+  hourly_income_target =  hourly_income + increase_per_hour
+  increase_per_customer =  (hourly_income_target/customers_per_hour) - avg_price
+  pct_increase_customer = (increase_per_customer/avg_price) * 100
   
   print "Current Minimum Wage = ${}".format(wage)
   print "New Minimum Wage = ${}".format(new_wage)
@@ -25,12 +26,12 @@ def wage_increase(wage, new_wage, employees, avg_price, turnover):
   print "Number of employees = {}".format(employees)
   print "Total Increased wage cost per hour = ${}".format(increase_per_hour)
   print "Average price per transaction = ${}".format(avg_price)
-  print "Customers per hour = {}".format(turnover)
-  print "Curent Hourly Income: ${}".format(hour_income)
-  print "New revenue target = ${}".format(new_revenue_target)
+  print "Customers per hour = {}".format(customers_per_hour)
+  print "Curent Hourly Income: ${}".format(hourly_income)
+  print "New revenue target = ${}".format(hourly_income_target)
   print "Increase per customer = ${}".format(increase_per_customer)
   print "Percentage Increase per customer = %{}".format(pct_increase_customer)
 
 
 if __name__ == "__main__":
-  wage_increase(wage, new_wage, employees, avg_price, turnover)
+  wage_increase(wage, new_wage, employees, avg_price, customers_per_hour)
