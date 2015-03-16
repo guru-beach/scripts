92a2a8ba6688d001ac131bf712edb0bf0b8fab29
Jake Lundberg <jlundberg@adconion.com>
2015-03-16 13:17:10 -0700

    master: float formating for sub-one numbers

--- wage_increase.py
+++ wage_increase.py
@@ -5,23 +5,23 @@ wage = 9.32
 # New minimum wage
 new_wage = 15
 # Employees working
-employees = 20
+employees = 3
 # Average price per transaction
 # Units are monetary (e.g. $)
-avg_price = 40
+avg_price = 4
 # Customers per hour
 customers_per_hour = 20
 # Additional support manhours per day
 # This adds in downstream cost for services provided by outside vendors
-downstream_manhours_per_day = 30
+downstream_manhours_per_day = 5
 operating_hours = 8
 
 def wage_increase(wage, new_wage, employees, avg_price, customers_per_hour, downstream_manhours_per_day, operating_hours):
-  wage_delta = (new_wage - wage)
-  downstream_manhours_per_hour = downstream_manhours_per_day/operating_hours
-  internal_increase_per_hour = wage_delta * employees
-  downstream_increase_per_hour = wage_delta * downstream_manhours_per_hour
-  increase_per_hour = internal_increase_per_hour + downstream_increase_per_hour
+  wage_delta = float(new_wage - wage)
+  downstream_manhours_per_hour = float(downstream_manhours_per_day)/float(operating_hours)
+  internal_increase_per_hour = float(wage_delta * employees)
+  downstream_increase_per_hour = float(wage_delta * downstream_manhours_per_hour)
+  increase_per_hour = float(internal_increase_per_hour + downstream_increase_per_hour)
   hourly_income = customers_per_hour * avg_price
   hourly_income_target =  hourly_income + increase_per_hour
   increase_per_customer =  (hourly_income_target/customers_per_hour) - avg_price
@@ -32,13 +32,14 @@ def wage_increase(wage, new_wage, employees, avg_price, customers_per_hour, down
   print "Wage difference per hour           = ${}".format(wage_delta)
   print "Number of employees                = {}".format(employees)
   print "Additional downstream manhours     = {}".format(downstream_manhours_per_day)
-  print "Total Increased wage cost per hour = ${}".format(increase_per_hour)
+  print "Downstream wage cost per hour      = ${:03.2f}".format(downstream_increase_per_hour)
+  print "Total Increased wage cost per hour = ${:03.2f}".format(increase_per_hour)
   print "Average price per transaction      = ${}".format(avg_price)
   print "Customers per hour                 = {}".format(customers_per_hour)
   print "Curent Hourly Income               = ${}".format(hourly_income)
-  print "New revenue target                 = ${}".format(hourly_income_target)
-  print "Increase per customer              = ${}".format(increase_per_customer)
-  print "Percentage Increase per customer   = %{}".format(pct_increase_customer)
+  print "New revenue target                 = ${:03.2f}".format(hourly_income_target)
+  print "Increase per customer              = ${:03.2f}".format(increase_per_customer)
+  print "Percentage Increase per customer   = %{:03.2f}".format(pct_increase_customer)
 
 
 if __name__ == "__main__":
