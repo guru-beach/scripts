ea6d14e6a70d601d7d8263128b6031d00b813780
Jake Lundberg <jlundberg@adconion.com>
2015-03-16 11:04:54 -0700

    master: fixing readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scripts
 Generic place to put arbitrary helper scripts
+
+## wage_increase.py
+Model for first tier price per customer increases based on wage increases.   Mainly created to show the effects of minimum wage increases on end customer prices.   Doesn't take into effect trickle up from business services.   
--- wage_increase.py
+++ wage_increase.py
@@ -1,24 +1,31 @@
+# Calculate the increase per customer if all wage increases are passed on
+
+# Current minimum wage
 wage = 9.32
+# New minimum wage
 new_wage = 15
-employees = 3
-avg_meal = 4
+# Employees working
+employees = 10
+# Average price transaction
+avg_price = 40
+# Customers per hour
 turnover = 20
 
-def main(wage, new_wage, employees, avg_meal, turnover):
+def wage_increase(wage, new_wage, employees, avg_price, turnover):
   wage_delta = (new_wage - wage)
   increase_per_hour =  (new_wage - wage) * employees
-  hour_income = turnover * avg_meal
+  hour_income = turnover * avg_price
   new_revenue_target =  hour_income + increase_per_hour
-  increase_per_customer =  new_revenue_target/turnover - avg_meal
-  pct_increase_customer = increase_per_customer/avg_meal * 100
+  increase_per_customer =  new_revenue_target/turnover - avg_price
+  pct_increase_customer = increase_per_customer/avg_price * 100
   
   print "Current Minimum Wage = ${}".format(wage)
   print "New Minimum Wage = ${}".format(new_wage)
   print "Wage difference per hour: ${}".format(wage_delta)
   print "Number of employees = {}".format(employees)
-  print "Increased salary cost per hour = ${}".format(increase_per_hour)
-  print "Average cost per meal = ${}".format(avg_meal)
-  print "Customer turnover per hour = {}".format(turnover)
+  print "Total Increased wage cost per hour = ${}".format(increase_per_hour)
+  print "Average price per transaction = ${}".format(avg_price)
+  print "Customers per hour = {}".format(turnover)
   print "Curent Hourly Income: ${}".format(hour_income)
   print "New revenue target = ${}".format(new_revenue_target)
   print "Increase per customer = ${}".format(increase_per_customer)
@@ -26,4 +33,4 @@ def main(wage, new_wage, employees, avg_meal, turnover):
 
 
 if __name__ == "__main__":
-  main(wage, new_wage, employees, avg_meal, turnover)
+  wage_increase(wage, new_wage, employees, avg_price, turnover)
