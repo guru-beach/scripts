3152660cc6cbe7100d1703e393d26f80141f0d2e
Jake Lundberg <code@guru-beach.com>
2015-03-21 12:14:05 -0700

    master: splitting out data sources and labelling

--- labor_api.py
+++ labor_api.py
@@ -1,9 +1,14 @@
 import requests
 import json
-start_year = '1947'
-end_year   = '2015'
+import datetime
+import numpy as np
+
 headers = {'Content-type': 'application/json'}
 rest_endpoint = 'http://api.bls.gov/publicAPI/v1/timeseries/data/'
+start_year = 1948
+stop_year = 2015
+interval = 10
+day = 1
 
 series_dict = { 'LNS14000000' : { 'name': 'Unemployment, Season Adjusted : US',
                                   'data': []
@@ -13,25 +18,41 @@ series_dict = { 'LNS14000000' : { 'name': 'Unemployment, Season Adjusted : US',
                                 }
               }
 
-series = series_dict.keys()
+def get_series_data(series_id, start_year, end_year):
+  """Takes a series ID, start year, end year and returns a list of series
+  tuples formatted as (datetime.date, float)"""
+  data_list = []
+  data = json.dumps({"seriesid": [series_id],"startyear": "{}".format(start_year), "endyear": "{}".format(end_year)})
+  p = requests.post(rest_endpoint, data=data, headers=headers)
+  json_data = json.loads(p.text)
+  # print p.text
+  for item in json_data['Results']['series'][0]['data']:
+    period = item['period']
+    year = int(item['year'])
+    month = int(period.replace('M',''))
+    value = float(item['value'])
+    data_list.append((datetime.date(year, month, day), value))
+  return data_list
+
+def split_years(start_year, stop_year, interval):
+  """Return a list of start/stop year doubles broken up into interval"""
+  start_stop = []
+  start_years = np.arange(start_year, stop_year, interval)
+  for start in start_years:
+    end = start + (interval - 1)
+    if end > stop_year:
+      end = stop_year
+    start_stop.append((start,end))
+  print start_stop
+  return start_stop
+
+ 
+for series_id in  series_dict.keys():
+  for start, stop in split_years(start_year, stop_year, interval):
+    #print "checking {} = {}".format(start, stop)
+    series_data = get_series_data(series_id, start, stop)
+    series_dict[series_id]['data'] += series_data
+
+print series_dict
 
 
-data = json.dumps({"seriesid": series,"startyear":start_year, "endyear":end_year})
-p = requests.post(rest_endpoint, data=data, headers=headers)
-json_data = json.loads(p.text)
-for series in json_data['Results']['series']:
-    seriesId = series['seriesID']
-    for item in series['data']:
-        year = item['year']
-        period = item['period']
-        month = period.replace('M','')
-        value = item['value']
-        footnotes=""
-        for footnote in item['footnotes']:
-            if footnote:
-                footnotes = footnotes + footnote['text'] + ','
-            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-    output = open(&quot;c:\\temp\\&quot; + seriesId + &quot;.txt&quot;,&quot;w&quot;)
-    output.write (x.get_string())
-    output.close()
-    
--- min_increase_v_unemployment.py
+++ min_increase_v_unemployment.py
@@ -1,26 +1,25 @@
 import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+from unemployment_dict import unemployment_dict as labor_data
+
+years = YearLocator()   # every year
+months = MonthLocator()  # every month
+yearsFmt = DateFormatter('%Y')
 
 d = datetime.date
 d2n = mdates.date2num
 
-labor_data {
-  'LNS14000000' : { 'name' : 'Seasonally Adjusted Unemployment: US',
-                    'data' : []
-                  }
-  'LNU04000000' : { 'name' : 'Not Seasonally Adjusted Unemployment: US',
-                    'data' : []
-                  }
-  'LAUMT534266000000006' : { 'name' : 'Not Seasonally Adjusted Unemployment: SEATAC'
-                             'data' : []
-                           }
+# Series can be found: http://download.bls.gov/pub/time.series/la/la.series
+series_info = { 
+'LNS14000000' : 'Seasonally Adjusted Unemployment: US',
+'LNU04000000' : 'Not Seasonally Adjusted Unemployment: US',
+'LASDV534264400000003' : 'Unemployment Rate: Seattle-Bellevue-Everett, WA Metropolitan Division',
 }
-
-def import_unemployment():
   
 raise_pct_1938 = [
-(d(1938, 10, 24), 100),
+(d(1938, 10, 24), 20),
 (d(1939, 10, 24), 20),
 (d(1945, 10, 24), 30),
 (d(1950, 1, 25), 85),
@@ -34,7 +33,7 @@ raise_pct_1938 = [
 ]
 
 raise_pct_1961 = [
-(d(1961, 9, 3), 100),
+(d(1961, 9, 3), 20),
 (d(1964, 9, 3), 15),
 (d(1965, 9, 3), 9),
 (d(1967, 2, 1), 15),
@@ -45,7 +44,7 @@ raise_pct_1961 = [
 ]
 
 raise_pct_1966_nonfarm = [
-(d(1967, 2, 1), 100),
+(d(1967, 2, 1), 20),
 (d(1968, 2, 1), 15),
 (d(1969, 2, 1), 13),
 (d(1970, 2, 1), 11.5),
@@ -57,7 +56,7 @@ raise_pct_1966_nonfarm = [
 ]
 
 raise_pct_1966_farm = [
-(d(1967, 2, 1), 100),
+(d(1967, 2, 1), 20),
 (d(1968, 2, 1), 15),
 (d(1969, 2, 1), 13),
 (d(1974, 5, 1), 23),
@@ -79,3 +78,70 @@ raise_pct_1977 = [
 (d(2008, 9, 24), 12),
 (d(2009, 9, 24), 10.7)
 ]
+
+
+# These don't need different x's in the list comprehension, but 
+# Making is easier for other people to understand what these are for
+raise_1938_dates = [x1[0] for x1 in  raise_pct_1938]
+raise_1938_pct = [y1[1] for y1 in  raise_pct_1938]
+
+raise_1961_dates = [x2[0] for x2 in raise_pct_1961]
+raise_1961_pct = [y2[1] for y2 in raise_pct_1961]
+
+raise_1966_nonfarm_dates = [x3[0] for x3 in raise_pct_1966_nonfarm]
+raise_1966_nonfarm_pct = [y3[1] for y3 in raise_pct_1966_nonfarm]
+
+raise_1966_farm_dates = [x4[0] for x4 in raise_pct_1966_farm]
+raise_1966_farm_pct = [y4[1] for y4 in raise_pct_1966_farm]
+
+raise_1977_dates = [x5[0] for x5 in raise_pct_1977]
+raise_1977_pct = [y5[1] for y5 in raise_pct_1977]
+
+# Seasonally Adjust Unemployment
+ue_sa = labor_data['LNS14000000']['data']
+ue_sa.sort()
+ue_sa_dates = [x6[0] for x6 in ue_sa]
+ue_sa_pct = [y6[1] for y6 in ue_sa]
+
+# Seasonally Adjust Unemployment
+ue_nsa = labor_data['LNU04000000']['data']
+ue_nsa.sort()
+ue_nsa_dates = [x7[0] for x7 in ue_nsa]
+ue_nsa_pct = [y7[1] for y7 in ue_nsa]
+
+# Seasonally adjusted Unemployment Seattle area
+#ue_sa_seattle = labor_data['LASDV534264400000003']['data']
+#ue_sa_seattle.sort()
+#ue_sa_seattle_dates = [x8[0] for x8 in ue_sa_seattle]
+#ue_sa_seattle_pct = [y8[1] for y8 in ue_sa_seattle]
+
+
+
+fig, ax1 = plt.subplots()
+bar1 = ax1.bar(raise_1938_dates, raise_1938_pct, width=10, color='k', edgecolor='k', label='1938 Law')
+bar2 = ax1.bar(raise_1961_dates, raise_1961_pct, width=10, color='m', edgecolor='m', label='1961 Law')
+bar3 = ax1.bar(raise_1966_nonfarm_dates, raise_1966_nonfarm_pct, width=10, color='c', edgecolor='c', label='1966 Nonfarm Law')
+bar4 = ax1.bar(raise_1966_farm_dates, raise_1966_farm_pct, width=10, color='y', edgecolor='y', label='1966 Farm Law')
+bar5 = ax1.bar(raise_1977_dates, raise_1977_pct, width=10, color='b', edgecolor='b', label='1977 Law')
+ax1.xaxis_date()
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Minimum Wage Raise %', color='b')
+ax1.xaxis.set_major_locator(years)
+ax1.xaxis.set_major_formatter(yearsFmt)
+ax1.xaxis.set_minor_locator(months)
+for tl in ax1.get_yticklabels():
+    tl.set_color('b')
+ax1.grid(which='both', color='green')
+ax1.autoscale_view()
+ax1.legend(loc=2)
+
+ax2 = ax1.twinx()
+line1 = ax2.plot(ue_sa_dates, ue_sa_pct,  'r-', label='US Unemployment Rate: Seasonally Adjusted')
+line2 = ax2.plot(ue_nsa_dates, ue_nsa_pct,  'g-', label='US Unemployment Rate: Non-Seasonally Adjusted')
+#line3 = ax2.plot(ue_sa_seattle_dates, ue_sa_seattle_pct,  'g-')
+ax2.set_ylabel('Unemployment %', color='r')
+for tl in ax2.get_yticklabels():
+    tl.set_color('r')
+ax2.legend(loc=1)
+
+plt.show()
